31155a179b6f3d597da463b207d98e878964eba5
AlonePainPeanut <139093156+noturcmd@users.noreply.github.com>
2024-12-27 19:14:11 +0700

    push

--- business-logic/validate-login.php
+++ business-logic/validate-login.php
@@ -1,51 +1,53 @@
 <?php
 
-function validateLogin($con)
-{
-    if (isset($_POST["login"])) {
-        $email = $_POST["email"];
-        $password = $_POST["password"];
 
-        $query = "SELECT * FROM users WHERE email='$email'";
-        $stm = $con->prepare($query);
-        $stm->execute();
-        $checkUser = $stm->fetch(PDO::FETCH_ASSOC);
-        if ($checkUser) {
-            if (password_verify($password, $checkUser["password"])) {
-                setcookie("login", "success", time() + (86400 * 7), "/");
-                setcookie("logusid", $checkUser["id"], time() + (86400 * 7), "/");
-                setcookie("logusmulmed", "osasuvu%/45342290pp" . $checkUser["id"], time() + (86400 * 7), "/");
-                $con = null;
-                echo <<<ALERT
-                <script>
-                alert('Login Berhasil!');
-                document.location.href = "../index.php";
-                </script>
-                ALERT;
-                exit();
-            } else {
-                $con = null;
-                echo <<<ALERT
-                <script>
-                alert('Password salah!');
-                document.location.href = "../login.php";
-                </script>
-                ALERT;
-                exit();
-            }
-        } else {
-            $con = null;
+include "../connection/db_connection.php";
+
+$koneksi = getConnection();
+
+if (isset($_POST["login"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $query = "SELECT * FROM users WHERE email='$email'";
+    $stm = $koneksi->prepare($query);
+    $stm->execute();
+    $checkUser = $stm->fetch(PDO::FETCH_ASSOC);
+    if ($checkUser) {
+        if (password_verify($password, $checkUser["password"])) {
+            setcookie("login", "success", time() + (86400 * 7), "/");
+            setcookie("logusid", $checkUser["id"], time() + (86400 * 7), "/");
+            setcookie("logusmulmed", "osasuvu%/45342290pp" . $checkUser["id"], time() + (86400 * 7), "/");
+            $koneksi = null;
             echo <<<ALERT
-            <script>
-            alert('Login gagal! Email tidak terdaftar');
-            document.location.href = "../login.php";
-            </script>
-            ALERT;
+                <script>
+                    alert('Login Berhasil!');
+                    document.location.href = "../index.php";
+                </script>
+                ALERT;
+            exit();
+        } else {
+            $koneksi = null;
+            echo <<<ALERT
+                <script>
+                    alert('Password salah!');
+                    document.location.href = "../login.php";
+                </script>
+                ALERT;
             exit();
         }
     } else {
-        $con = null;
-        header("location: ../login.php");
+        $koneksi = null;
+        echo <<<ALERT
+                <script>
+                    alert('Login gagal! Email tidak terdaftar');
+                    document.location.href = "../login.php";
+                </script>
+                ALERT;
         exit();
     }
+} else {
+    $koneksi = null;
+    header("location: ../login.php");
+    exit();
 }
--- business-logic/validate-signup.php
+++ business-logic/validate-signup.php
@@ -1,77 +1,79 @@
 <?php
 
-function validateSignUp($con)
-{
-    if (isset($_POST["signup"])) {
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $password = $_POST["password"];
+include "../connection/db_connection.php";
 
-        $query = "SELECT email FROM users WHERE email='$email'";
-        $stm = $con->prepare($query);
+$koneksi = getConnection();
+
+if (isset($_POST["signup"])) {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $query = "SELECT email FROM users WHERE email='$email'";
+    $stm = $koneksi->prepare($query);
+    $stm->execute();
+    $checkUser = $stm->fetch(PDO::FETCH_ASSOC);
+
+    if ($checkUser) {
+        $koneksi = null;
+        echo <<<ALERT
+        <script>
+            alert('Email sudah digunakan!');
+            document.location.href = "../signup.php";
+        </script>
+        ALERT;
+        exit();
+    } else {
+        $query = "SELECT LOWER(username) FROM users WHERE username='$username'";
+        $stm = $koneksi->prepare($query);
         $stm->execute();
         $checkUser = $stm->fetch(PDO::FETCH_ASSOC);
-
         if ($checkUser) {
-            $con = null;
+            echo "user sudah ada!";
+            $koneksi = null;
             echo <<<ALERT
             <script>
-                alert('Email sudah digunakan!');
+                alert('Username sudah digunakan!');
                 document.location.href = "../signup.php";
             </script>
             ALERT;
             exit();
         } else {
-            $query = "SELECT LOWER(username) FROM users WHERE username='$username'";
-            $stm = $con->prepare($query);
-            $stm->execute();
-            $checkUser = $stm->fetch(PDO::FETCH_ASSOC);
-            if ($checkUser) {
-                echo "user sudah ada!";
-                $con = null;
+            $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+            $stm = $koneksi->prepare($query);
+
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            // Bind parameter
+            $stm->bindParam(':username', $username);
+            $stm->bindParam(':email', $email);
+            $stm->bindParam(':password', $hashedPassword);
+
+            // Eksekusi query
+            if ($stm->execute()) {
+                $koneksi = null;
                 echo <<<ALERT
                 <script>
-                alert('Username sudah digunakan!');
-                document.location.href = "../signup.php";
+                    alert('Registrasi berhasil! Silakan login.');
+                    document.location.href = "../login.php";
                 </script>
                 ALERT;
                 exit();
             } else {
-                $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
-                $stm = $con->prepare($query);
-                
-                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-                
-                // Bind parameter
-                $stm->bindParam(':username', $username);
-                $stm->bindParam(':email', $email);
-                $stm->bindParam(':password', $hashedPassword);
-                
-                // Eksekusi query
-                if ($stm->execute()) {
-                    $con = null;
-                    echo <<<ALERT
-                    <script>
-                    alert('Registrasi berhasil! Silakan login.');
-                    document.location.href = "../login.php";
-                    </script>
-                    ALERT;
-                    exit();
-                } else {
-                    $con = null;
-                    echo <<<ALERT
-                    <script>
+                $koneksi = null;
+                echo <<<ALERT
+                <script>
                     alert('Terjadi kesalahan, coba lagi nanti.');
                     document.location.href = "../signup.php";
-                    </script>
-                    ALERT;
-                    exit();
-                }
+                </script>
+                ALERT;
+                exit();
             }
         }
-    } else {
-        $con = null;
-        header("location: ../login.php");
-        exit();
     }
+} else {
+    header("location: ../login.php");
+    exit();
 }
+
+$koneksi = null;
